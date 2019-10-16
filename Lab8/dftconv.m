fff6667170de490a71bbb7016c10f7b5ff898565
Ahish9009 <ahishjayant9009@gmail.com>
2019-10-16 11:37:14 +0530

    Q4 added

---
+++ Lab8/dftconv.m
@@ -0,0 +1,39 @@
+function [conv_circ, conv_lin] = dftconv(h,x,N); 
+	lx = length(x);
+	lh = length(h);
+	if N<max([lx lh])
+		error('N must be at least the length of the longer signal.')
+       	end
+	
+	h_circ = [h, zeros(1,N-lh)];
+	x_circ = [x, zeros(1,N-lx)];
+	% Compute order-N circular convolution based on length-N FFTs 
+	conv_circ = ifft(fft(h_circ).*fft(x_circ))
+	% Compute linear convolution based on appropriate-length FFTs/zero padding
+	h_lin = [h, zeros(1,lx-1)];
+	x_lin = [x, zeros(1, lh-1)];
+	conv_lin = ifft(fft(h_lin).*fft(x_lin))
+
+	convAns = conv(x, h);
+	cconvAns = cconv(x, h, N);
+
+	subplot(2,2,1);
+	plot(conv_circ);
+
+	subplot(2,2,2);
+	plot(cconvAns);
+
+	subplot(2,2,3);
+	plot(conv_lin);
+
+	subplot(2,2,4);
+	plot(convAns);
+
+	% Complexity of fft and ifft functions is NlogN, but element wise multiplication of the two fft matrics(of order N) will be O(N)
+	% Thus, overall complexity is O(NlogN)
+
+	%Complexity of convolution in time domain is O(N^2)
+
+
+end
+

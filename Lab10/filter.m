3001328eae8fd54571e90a9ee638340df42f492d
Shivansh Rakesh <49039969+ShivanshRakesh@users.noreply.github.com>
2019-10-30 12:36:10 +0530

    added filter code

---
+++ Lab10/filter.m
@@ -0,0 +1,25 @@
+function [hl, hf] = filter1(len)
+    wc = 0.4*pi;
+    hd = zeros(1, len);
+    for i = 1:len
+        if(i == (len-1)./2)
+            hd(i) = wc./pi;
+        else
+            hd(i) = sin(wc.*(i - (len-1)./2))./(pi*(i - (len-1)./2));
+        end
+    end
+    w = blackman(len);
+    h = hd.*transpose(w);
+    hl = fftshift(fft(h, 51));
+    subplot(121);
+    plot(abs(hl));
+    
+    for i = 1:len
+        hd(i) = hd(i) .* (-1).^i;
+    end
+    w = blackman(len);
+    h = hd.*transpose(w);
+    hf = fftshift(fft(h, 51));
+    subplot(122);
+    plot(abs(hf));
+end

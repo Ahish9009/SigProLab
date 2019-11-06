14e829aa44acd758a6ce912438941202e65615eb
Ahish9009 <ahishjayant9009@gmail.com>
2019-11-06 16:34:09 +0530

    q1 done done

--- Lab12/checkfilter.m
+++ Lab12/checkfilter.m
@@ -5,22 +5,40 @@ function y = checkfilter()
 
 	%generates signal	
 	t = 0:1:300;
-	s1 = sin(w0*t)
-	s2 = sin((2*w0).*t)
+	s1 = sin(w0*t);
+	s2 = sin((2*w0).*t);
 
 	combined = s1 + s2;
 	%combined = combined./max(combined);
-	%plot(abs(fft(combined)));
 
+	%gets the b and a matrix from notch function
 	[b, a] = notch(w0, 0.95);
 
+	%filters the signal using the values found from the notch function
 	filtered = filter(b,a,combined);
+
+	%plotting
 	subplot(221);
 	plot(abs(fftshift(fft(combined))));
+	title("Combined Signal FFT")
+	xlabel("w");
+	ylabel("Magnitude")
+
 	subplot(222);
 	plot(abs(fftshift(fft(filtered))));
+	title("Filtered Signal FFT")
+	xlabel("w");
+	ylabel("Magnitude")
+
 	subplot(223);
 	plot(t, combined);
+	title("Combined Signal in Time Domain")
+	xlabel("t");
+	ylabel("x(t)");
+
 	subplot(224);
 	plot(t, filtered);
+	title("Filtered Signal in Time Domain")
+	xlabel("t");
+	ylabel("x(t)");
 end
--- Lab12/notch.m
+++ Lab12/notch.m
@@ -4,10 +4,10 @@ function [b, a] = notch(w0, r)
 	b = [1 -(exp(1i*w0) + exp(-1i*w0)) 1];
 	% Compute denominator polynomial from r and w0
 	a = [1 -r*(exp(1i*w0) + exp(-1i*w0)) r*r];
-
 	% Determine gain so that frequency response has magnitude 1 at w0
 	b = b.*(1 - 2.*r.*cos(w0) + r.*r)./(2 - 2.*cos(w0));
 	% Diagnostic display
 	freqz(b,a);
+	title("Frequency Response")
 	%fvtool(b,a);
 end

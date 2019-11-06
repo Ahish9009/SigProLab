4b7bfa81e71f3f1720c518c40816c95889009bae
Ahish9009 <ahishjayant9009@gmail.com>
2019-11-06 11:26:04 +0530

    Check filter

--- Lab12/checkfilter.m
+++ Lab12/checkfilter.m
@@ -1,21 +1,26 @@
 function y = checkfilter()
 
-	w0 = 0.2.*pi;
+	w0 = 0.4.*pi;
 
-	t = 0:1:30;
+	t = 0:1:5000;
 	s1 = sin(w0*t)
 	s2 = sin((2*w0).*t)
 
 	combined = s1 + s2;
-	plot(abs(fft(combined)));
+	combined = combined./max(combined);
+	%plot(abs(fft(combined)));
 
 	[b, a] = notch(2*w0, 0.99)
 
-	y = filter(b,a,combined);
-	%plot(abs(fft(combined)));
-	%plot(abs(fft(y)));
-	%subplot(121);
-	%plot(t, combined);
-	%subplot(122);
-	%plot(t, y);
+	%freqz(b,a);
+
+	filtered = filter(a,b,combined);
+	subplot(221);
+	plot(abs(fft(combined)));
+	subplot(222);
+	plot(abs(fft(filtered)));
+	subplot(223);
+	plot(t, combined);
+	subplot(224);
+	plot(t, filtered);
 end
